a0b3a65189a3ec0ee55676612a594053ebaca9c4
Anatoli Makarevich <makaroni4@gmail.com>
2015-12-08 21:22:29 +0100

    13 Solved

--- 13 Wires/solution.js
+++ 13 Wires/solution.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var operations = fs.readFileSync("input").toString().trim().split("\n");
 
 var inputs = {}
+var signals = {}
 
 operations.forEach(function(operation) {
   var operation = operation.split(" -> ");
@@ -23,7 +24,58 @@ operations.forEach(function(operation) {
     return el !== "";
   })
 
-  console.log(operands)
-  console.log(command)
-  console.log(output)
+  inputs[output] = {
+    operands: operands,
+    command: command
+  };
 });
+
+function getOperandValue(operand) {
+  if(operand.match(/^\d+$/)) {
+    return parseInt(operand);
+  } else if(operand.match(/^[a-z]+$/)) {
+    return signals[operand];
+  }
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function calculateSignals() {
+  Object.keys(inputs).forEach(function(key, index) {
+    var inputData = inputs[key];
+    var operands = inputData.operands.map(function(el, index) {
+      return getOperandValue(el);
+    });
+
+    if(operands.every(function(n) { return isNumeric(n); })) {
+      switch(inputData.command) {
+        case null:
+          signals[key] = operands[0];
+          break;
+        case "AND":
+          signals[key] = operands[0] & operands[1];
+          break;
+        case "OR":
+          signals[key] = operands[0] | operands[1]
+          break;
+        case "LSHIFT":
+          signals[key] = operands[0] << operands[1]
+          break;
+        case "RSHIFT":
+          signals[key] = operands[0] >> operands[1]
+          break;
+        case "NOT":
+          signals[key] = ~operands[0]
+          break;
+      }
+    }
+  });
+}
+
+while(Object.keys(signals).length < Object.keys(inputs).length) {
+  calculateSignals();
+}
+
+console.log(signals["a"]);
